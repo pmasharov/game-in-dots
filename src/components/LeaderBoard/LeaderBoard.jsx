c834f87c8f8ee8299cea30dc7677cd97016a9b69
pmasharov <Mpa18912518>
2020-11-16 19:31:56 +0300

    collapse leaders board

--- src/components/LeaderBoard/LeaderBoard.jsx
+++ src/components/LeaderBoard/LeaderBoard.jsx
@@ -1,4 +1,4 @@
-import React, { useEffect } from 'react'
+import React, { useEffect, useState } from 'react'
 import { connect } from 'react-redux'
 
 import api from '../../api'
@@ -7,6 +7,13 @@ const LeaderBoard = ({
                        leadersData,
                        dispatch,
                      }) => {
+  const [seeAll, changeSeeAll] = useState(false)
+  const defaultVisibleResultsCount = 5;
+
+  const toggleSeeAll = () => {
+    changeSeeAll(!seeAll)
+  }
+
   const getLeadersData = async () => {
     const leadersData = await api.getLeadersData(dispatch)
     return leadersData
@@ -18,7 +25,12 @@ const LeaderBoard = ({
 
   const leadersContent = leadersData && !!leadersData.length && leadersData.map((leaderItem, index) => {
     const { winner, date } = leaderItem
-    return (
+    return !seeAll ? index < defaultVisibleResultsCount ? (
+      <li key={index} className="leader-board__content-item">
+        <span>{winner}</span>
+        <span>{date}</span>
+      </li>
+    ) : null : (
       <li key={index} className="leader-board__content-item">
         <span>{winner}</span>
         <span>{date}</span>
@@ -30,7 +42,10 @@ const LeaderBoard = ({
       {leadersContent && <div className="leader-board">
         <section className="leader-board__header-wrapper">
           <h1 className="leader-board__header">
-            Leader Board
+            <span>Leader Board</span>
+            <span className='collapse-control' onClick={toggleSeeAll}>
+              {!seeAll ? 'See all results' : 'See less'}
+            </span>
           </h1>
         </section>
         <ul className="leader-board__content">
